baf8590cc024d15ffb7dffd215f6358022fd6516
ShobhanKS <ShobhanKS@DESKTOP-2BFARG8>
2019-08-30 17:37:05 +0530

    test

--- src/main/java/test/ReadPropertiesFile.java
+++ src/main/java/test/ReadPropertiesFile.java
@@ -1,11 +1,21 @@
 package test;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ReadPropertiesFile {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
+		FileReader reader = new FileReader(System.getProperty("user.dir")+"/src/main/resources/PropertyFile/application.properties");
 		
 
+		Properties properties = new Properties();
+		properties.load(reader);
+
+		System.out.println(properties.getProperty("user"));
+		System.out.println(properties.getProperty("password"));
 	}
 
 }
